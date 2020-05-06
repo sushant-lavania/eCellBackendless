b2ad43c41fce016f39948c17b6723aed6afe6919
Simranjeet Singh <sjsingh9090@gmail.com>
2020-05-07 04:53:36 +0530

    domain Changed

---
+++ js/google-map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 28.6091, lng: 77.0347};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
